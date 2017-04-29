9de2be2de9ebdbdd6850f9802b9f3adc2a343d4f
mike_neck <jkrt3333@gmail.com>
2017-04-30 00:09:57 +0900

    Tree(Set)の実装

---
+++ purely-functional-data-structure/src/Persistent/Tree.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Persistent.Tree where
+
+class Set s where
+  empty :: (Ord a) => s a
+  insert:: (Ord a) => a -> s a -> s a
+  member:: (Ord a) => a -> s a -> Bool
+
+data Tree a = Empty | Tree (Tree a) a (Tree a)
+  deriving Show
+
+instance Set Tree where
+  empty:: (Ord a) => Tree a
+  empty = Empty
+
+  member:: (Ord a) => a -> Tree a -> Bool
+  member _ Empty        = False
+  member x (Tree l y r)
+    | x < y     = member x l
+    | x > y     = member x r
+    | otherwise = True
+
+  insert:: (Ord a) => a -> Tree a -> Tree a
+  insert x Empty        = Tree Empty x Empty
+  insert x t@(Tree l y r)
+    | x < y     = Tree (insert x l) y r
+    | x > y     = Tree l y $ insert x r
+    | otherwise = t
+
+treeOf:: (Ord a) => [a] -> Tree a
+treeOf xs = foldl (flip insert) Empty xs
