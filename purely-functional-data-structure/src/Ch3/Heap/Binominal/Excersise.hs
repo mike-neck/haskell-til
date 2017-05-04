1c2f4382d3c3205e3b63c683101f0cded11c3ed7
mike_neck <jkrt3333@gmail.com>
2017-05-04 12:09:18 +0900

    演習3.6実装完了

--- purely-functional-data-structure/src/Ch3/Heap/Binominal/Excersise.hs
+++ purely-functional-data-structure/src/Ch3/Heap/Binominal/Excersise.hs
@@ -30,3 +30,44 @@ instance Heap BinHeap where
   isEmpty = null . elems
 
   insert x xs = insTree (0, Node x []) xs
+
+  merge (BinHeap l) (BinHeap r) = BinHeap $ go l r
+    where
+      go:: (Ord a) => [(Int, Tree a)] -> [(Int, Tree a)] -> [(Int, Tree a)]
+      go l [] = l
+      go [] r = r
+      go l@(x:xs) r@(y:ys)
+        | rx < ry       = x : go xs r
+        | rx > ry       = y : go l ys
+        | otherwise     = insTreeRaw (rx, linked) $ go xs ys
+          where
+            rx = fst x
+            ry = fst y
+            linked = link x' y'
+            x' = snd x
+            y' = snd y
+
+  findMin (BinHeap ts)      = go ts
+    where
+      go []         = Nothing
+      go [(_,x)]    = Just $ root x
+      go ((_,x):xs) = do
+        y <- go xs
+        if x' <= y
+          then Just x'
+          else Just y
+          where
+            x' = root x
+
+  deleteMin (BinHeap ts) = fmap BinHeap <$> go ts
+    where
+      go:: (Ord a) => [(Int, Tree a)] -> Maybe (a, [(Int, Tree a)])
+      go []             = Nothing
+      go [(_,x)]        = Just (root x, [])
+      go ((_,x):ts)  = do
+        (y, ys) <- go ts
+        if x' <= y
+        then Just (x', ts)
+        else Just (y, ys)
+        where
+          x' = root x
