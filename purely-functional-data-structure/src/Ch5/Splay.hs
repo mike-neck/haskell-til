3af10ffad0e249a5ea232ef08671d8b4dc1c6a7d
mike_neck <jkrt3333@gmail.com>
2017-05-09 08:18:19 +0900

    演習5.4を実装

--- purely-functional-data-structure/src/Ch5/Splay.hs
+++ purely-functional-data-structure/src/Ch5/Splay.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Ch5.Splay where
 
 data Splay a = Empty | Tree (Splay a) a (Splay a)
@@ -7,7 +9,12 @@ instance (Show a) => Show (Splay a) where
   show (Tree l x r) =
     "[" ++ show x ++ " l:" ++ show l ++ ", r:" ++ show r ++ "]"
 
-bigger:: (Ord a) => a -> Splay a -> Splay a
+type Partition a = (Ord a) => a -> Splay a -> Splay a
+
+single:: (Ord a) => a -> Splay a
+single x = Tree Empty x Empty
+
+bigger:: Partition a
 bigger _ Empty        = Empty
 bigger p (Tree l x r)
   | x <= p    = bigger p r
@@ -17,4 +24,13 @@ bigger p (Tree l x r)
       then Tree (bigger p lr) x r
       else Tree (bigger p ll) y $ Tree lr x r
 
-
+smaller:: Partition a
+smaller _ Empty = Empty
+smaller p (Tree l x r)
+  | p < x       = smaller p l
+  | otherwise   = case r of
+    Empty         -> Tree l x Empty
+    Tree rl y rr  ->
+      if p < y
+        then Tree l x $ smaller p rl
+        else Tree (Tree l x rl) y $ smaller p rr
