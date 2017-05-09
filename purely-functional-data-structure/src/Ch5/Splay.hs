b166ee47ff2f75ad5569a5cb35384ea76eef9e7f
mike_neck <jkrt3333@gmail.com>
2017-05-09 23:17:01 +0900

    partition関数を作成

--- purely-functional-data-structure/src/Ch5/Splay.hs
+++ purely-functional-data-structure/src/Ch5/Splay.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DeriveFunctor #-}
 
 module Ch5.Splay where
 
 data Splay a = Empty | Tree (Splay a) a (Splay a)
+  deriving Functor
 
 instance (Show a) => Show (Splay a) where
   show Empty = "[]"
@@ -34,3 +36,27 @@ smaller p (Tree l x r)
       if p < y
         then Tree l x $ smaller p rl
         else Tree (Tree l x rl) y $ smaller p rr
+
+partition:: (Ord a) => a -> Splay a -> (Splay a, Splay a)
+partition _ Empty = (Empty, Empty)
+partition p t@(Tree l x r)
+  | x <= p    = case r of
+    Empty         -> (t, Empty)
+    Tree rl y rr  -> if y <= p
+      then let
+        (s,b) = partition p rr
+        tl = Tree l x rl
+        in (Tree tl y s, b)
+      else let
+        (s,b) = partition p rl
+        in (Tree l x s, Tree b y rr)
+  | otherwise = case l of
+    Empty         -> (Empty, t)
+    Tree ll y lr  -> if y <= p
+      then let
+        (s,b) = partition p lr
+          in (Tree ll y s, Tree b x r)
+      else let
+        (s,b) = partition p ll
+        tr = Tree lr x r
+          in (s, Tree b y tr)
