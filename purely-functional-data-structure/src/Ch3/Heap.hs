0186e0ce16d3861e4cd30ebfe231955993dbee3e
mike_neck <jkrt3333@gmail.com>
2017-05-11 02:58:52 +0900

    演習3.7の意味がわかった

--- purely-functional-data-structure/src/Ch3/Heap.hs
+++ purely-functional-data-structure/src/Ch3/Heap.hs
@@ -1,5 +1,7 @@
 module Ch3.Heap where
 
+import Data.Maybe(fromMaybe)
+
 class Heap h where
   empty:: (Ord a) => h a
   isEmpty:: (Ord a) => h a -> Bool
@@ -9,3 +11,39 @@ class Heap h where
 
   findMin:: (Ord a) => h a -> Maybe a
   deleteMin:: (Ord a) => h a -> Maybe (a, h a)
+
+toHeap:: (Ord a, Heap h) => [a] -> h a
+toHeap = foldl (flip insert) empty
+
+data ExplicitMin h a = Vac | Oqp a (h a)
+
+instance (Heap h) => Heap (ExplicitMin h) where
+  empty = Vac
+  isEmpty Vac = True
+  isEmpty _   = False
+
+  insert x Vac        = Oqp x $ insert x (insert x empty)
+  insert x (Oqp y h)
+    | x < y     = Oqp x heap
+    | otherwise = Oqp y heap
+      where
+        heap = insert x h
+
+  merge Vac e = e
+  merge (Oqp x l) (Oqp y r)
+    | x < y     = Oqp x heap
+    | otherwise = Oqp y heap
+    where
+      heap = merge l r
+
+  findMin Vac       = Nothing
+  findMin (Oqp x _) = Just x
+
+  deleteMin Vac       = Nothing
+  deleteMin (Oqp x h) = Just (x, heap)
+    where
+      heap = fromMaybe Vac del
+      del = do
+        (_, h') <- deleteMin h
+        m <- findMin h'
+        return $ Oqp m h'
